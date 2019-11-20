eb261dccb48b1aff3973c04603bc6091b7e9ca6f
adityapp <axceofficial@gmail.com>
2019-11-20 21:33:50 +0700

    [adit | gifary] Simplify view API
    Changes:
     - Setup RecyclerView inside the view
     - Add user-customizable view attributes (spanCount)
     - Change the view base from View to FrameLayout
     - Add sample usage

--- squaremenu/src/main/java/com/bcc/opensource/squaremenu/SquareMenu.java
+++ squaremenu/src/main/java/com/bcc/opensource/squaremenu/SquareMenu.java
@@ -1,24 +1,45 @@
 package com.bcc.opensource.squaremenu;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
-import android.view.View;
+import android.widget.FrameLayout;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class SquareMenu extends View {
+import java.util.List;
+
+public class SquareMenu extends FrameLayout {
+    private static final int DEFAULT_SPAN_COUNT = 3;
+
     private SquareMenuAdapter adapter = new SquareMenuAdapter();
+    private int spanCount;
 
-    public SquareMenu(@NonNull Context context) {
-        super(context);
-    }
-
-    public SquareMenu(@NonNull Context context, @Nullable AttributeSet attrs) {
+    public SquareMenu(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        setupAttribute(attrs);
+        setupRecyclerView();
     }
 
-    public SquareMenu(@NonNull Context context, @Nullable AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+    private void setupRecyclerView() {
+        RecyclerView recyclerView = new RecyclerView(getContext());
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), spanCount));
+        recyclerView.setAdapter(adapter);
+        addView(recyclerView);
+    }
+
+    public void setMenuItems(List<MenuItem> menuItems) {
+        adapter.setMenuItems(menuItems);
+    }
+
+    private void setupAttribute(@Nullable AttributeSet attrs) {
+        TypedArray typedArray = getContext().getTheme()
+                .obtainStyledAttributes(attrs, R.styleable.SquareMenu, 0, 0);
+
+        spanCount = typedArray.getInt(R.styleable.SquareMenu_spanCount, DEFAULT_SPAN_COUNT);
+
+        typedArray.recycle();
     }
 }
--- squaremenu/src/main/java/com/bcc/opensource/squaremenu/SquareMenuAdapter.java
+++ squaremenu/src/main/java/com/bcc/opensource/squaremenu/SquareMenuAdapter.java
@@ -23,7 +23,7 @@ public class SquareMenuAdapter extends RecyclerView.Adapter<SquareMenuAdapter.Vi
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_menu_item, parent);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_menu_item, parent, false);
         return new ViewHolder(view);
     }
 
