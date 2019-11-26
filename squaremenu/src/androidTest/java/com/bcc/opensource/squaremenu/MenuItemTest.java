e7f945f300aba481a87c0273b4e54d8dfcb988df
Adam Akbar <graha.asa@gmail.com>
2019-11-26 19:28:46 +0700

    Add simple test

---
+++ squaremenu/src/androidTest/java/com/bcc/opensource/squaremenu/MenuItemTest.java
@@ -0,0 +1,43 @@
+package com.bcc.opensource.squaremenu;
+
+
+import android.graphics.drawable.Drawable;
+
+import androidx.annotation.NonNull;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MenuItemTest  {
+
+    private MenuItem  menuItem;
+
+    @Before
+    public void setUp()  {
+        menuItem = new MenuItem("Satu",
+                InstrumentationRegistry.getInstrumentation().getContext()
+                        .getDrawable(R.drawable.ic_launcher_background));
+
+    }
+
+    @Test
+    public void testMenuItemNotNull(){
+        assertNotNull(menuItem);
+    }
+
+    @Test
+    public void testMenuGetTittleShouldReturnString() {
+        assertEquals(menuItem.getTitle(),"Satu");
+    }
+
+    @Test
+    public void getImage()  {
+        Drawable image =  InstrumentationRegistry.getInstrumentation().getContext().getDrawable(R.drawable.ic_launcher_background);
+        if (image!=null) {
+            assertEquals(menuItem.getImage().getConstantState(), image.getConstantState());
+        }
+    }
+}
