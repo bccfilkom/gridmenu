b745103c94c8f8b0d1b355840501283c79daeca2
Adam Akbar <graha.asa@gmail.com>
2019-12-07 20:24:32 +0700

    Create Test for Menu Item and Square Menu Adapter (#9)

---
+++ gridmenuview/src/androidTest/java/com/bcc/gridmenuview/MenuItemTest.java
@@ -0,0 +1,42 @@
+package com.bcc.gridmenuview;
+import android.graphics.drawable.Drawable;
+import androidx.test.platform.app.InstrumentationRegistry;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MenuItemTest  {
+    private MenuItem  menuItem;
+
+    @Before
+    public void setUp()  {
+        menuItem = new MenuItem("Satu",InstrumentationRegistry
+                .getInstrumentation()
+                .getContext()
+                .getDrawable(R.drawable.ic_launcher_background));
+
+    }
+
+    @Test
+    public void menuItemNotNull(){
+        assertNotNull(menuItem);
+    }
+
+    @Test
+    public void menuGetTitleShouldReturnString() {
+        assertEquals("Satu", menuItem.getTitle());
+    }
+
+    @Test
+    public void getImage()  {
+        Drawable image =  InstrumentationRegistry
+                .getInstrumentation()
+                .getContext()
+                .getDrawable(R.drawable.ic_launcher_background);
+        if (image!=null) {
+            assertEquals(image.getConstantState() , menuItem.getImage()
+                    .getConstantState());
+        }
+    }
+}
---
+++ gridmenuview/src/androidTest/java/com/bcc/gridmenuview/SquareMenuAdapterTest.java
@@ -0,0 +1,43 @@
+package com.bcc.gridmenuview;
+import androidx.test.platform.app.InstrumentationRegistry;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import static org.junit.Assert.*;
+
+public class SquareMenuAdapterTest {
+
+    private GridMenuAdapter squareMenuAdapter;
+
+    @Before
+    public void setUp(){
+        squareMenuAdapter = new GridMenuAdapter();
+    }
+
+    @Test
+    public void constructorShouldNotReturnNull(){
+        assertNotNull(squareMenuAdapter);
+    }
+
+    @Test
+    public void sizeShouldReturnThreeAfterAddThreeItem(){
+        ArrayList<MenuItem> list = new ArrayList<>();
+        list.add(new MenuItem("satu", InstrumentationRegistry.
+                getInstrumentation().
+                getContext().
+                getResources().
+                getDrawable(R.drawable.ic_launcher_background)));
+        list.add(new MenuItem("dua", InstrumentationRegistry
+                .getInstrumentation()
+                .getContext()
+                .getResources()
+                .getDrawable(R.drawable.ic_launcher_background)));
+        list.add(new MenuItem("tiga", InstrumentationRegistry.getInstrumentation()
+                .getContext()
+                .getResources()
+                .getDrawable(R.drawable.ic_launcher_background)));
+        squareMenuAdapter.setMenuItems(list);
+        assertEquals(list.size(), squareMenuAdapter.getItemCount());
+    }
+}
