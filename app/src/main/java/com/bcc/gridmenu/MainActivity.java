d6f767d55271593757ac41e46316b25e55361bdb
Raditya Rinandyaswara <45010500+radityarin@users.noreply.github.com>
2020-06-14 14:45:29 +0700

    add load image uri and make item layout responsive (#24)
    * add load image uri and make item layout responsive
    
    * fix indentation assertEquals getImageUri on MenuItemTest
    
    * change Drawable in MenuItem to ImageProvider and create Provider for Drawable, Local, and Network drawable that implements ImageProvider
    
    * add previous constructor and modify the test
    
    * switch NetworkImageTask and LocalImageTask parameter position
    
    * move list declaration to inside oncreate

--- app/src/main/java/com/bcc/gridmenu/MainActivity.java
+++ app/src/main/java/com/bcc/gridmenu/MainActivity.java
@@ -8,23 +8,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.bcc.gridmenuview.GridMenu;
 import com.bcc.gridmenuview.event.OnItemClickListener;
 import com.bcc.gridmenuview.model.MenuItem;
+import com.bcc.gridmenuview.provider.DrawableImageProvider;
+import com.bcc.gridmenuview.provider.LocalImageProvider;
+import com.bcc.gridmenuview.provider.NetworkImageProvider;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DrawableImageProvider drawableImageProvider = new DrawableImageProvider(getResources().getDrawable(R.drawable.ic_launcher_background));
+        NetworkImageProvider networkImageProvider = new NetworkImageProvider(this, "https://cdn3.iconfinder.com/data/icons/capsocial-round/500/youtube3-128.png");
+        LocalImageProvider localImageProvider = new LocalImageProvider(this, "/storage/emulated/0/DCIM/Camera/test.jpg");
+
         final ArrayList<MenuItem> list = new ArrayList<>();
         list.add(new MenuItem("satu", getResources().getDrawable(R.drawable.ic_launcher_background)));
-        list.add(new MenuItem("dua", getResources().getDrawable(R.drawable.ic_launcher_background)));
-        list.add(new MenuItem("tiga", getResources().getDrawable(R.drawable.ic_launcher_background)));
-        list.add(new MenuItem("empat", getResources().getDrawable(R.drawable.ic_launcher_background)));
-        list.add(new MenuItem("lima", getResources().getDrawable(R.drawable.ic_launcher_background)));
-        list.add(new MenuItem("enam", getResources().getDrawable(R.drawable.ic_launcher_background)));
+        list.add(new MenuItem("dua", drawableImageProvider));
+        list.add(new MenuItem("tiga", networkImageProvider));
+        list.add(new MenuItem("empat", localImageProvider));
 
         GridMenu menu = findViewById(R.id.square_menu);
         menu.setMenuItems(list);
